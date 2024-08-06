ff125360c8ead823b46116e72261830494735be3
nikhil <nikhilpokuri143@gmail.com>
2024-08-06 20:30:45 +0530

    pushed

---
+++ studentReport.py
@@ -0,0 +1,11 @@
+""" 
+INPUTS:
+- name
+- english
+- maths
+- science
+- history
+"""
+name = input("Enter Name: ")
+english = float(input("Enter English Score: "))
+maths = float(input("Enter Maths Score: "))
